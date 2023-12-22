3062b0dab9f2648c76697970827a784349bd157c
eugenekang <ek.cloud.dev@gmail.com>
2023-12-22 15:41:54 -0500

    20231222: More refactoring to test suites

--- src/euler/euler_problems.py
+++ src/euler/euler_problems.py
@@ -10,35 +10,35 @@ class Euler:
     def __init__(self) -> None:
         self.logger = logging.getLogger(__name__)
 
-    def problem_1(self):
+    def problem_1(self, lower=1, upper=1000, multiple_a=3, multiple_b=5):
         # Instruction: Find the sum of all multiples of 3 or 5 below 1000
         problem_1_soln = CustomEulerFunction().sum_of_multiples(
-            lower=1,
-            upper=1000,
-            multiple_a=3,
-            multiple_b=5
+            lower=int(lower),
+            upper=int(upper),
+            multiple_a=int(multiple_a),
+            multiple_b=int(multiple_b)
         )
         return problem_1_soln
 
-    def problem_2(self):
+    def problem_2(self, fibonacci_lim=4000000):
         # Instruction: Find sum of even valued terms in Fibonacci sequence.
         problem_2_soln = CustomEulerFunction().get_sum_evens_fibonacci_seq(
-            fibonacci_lim=4000000
+            fibonacci_lim=int(fibonacci_lim)
         )
         return problem_2_soln
 
-    def problem_3(self):
+    def problem_3(self, num=600851475143):
         # Instruction: Largest Prime Factor of 600851475143
         problem_3_soln = CustomEulerFunction().get_largest_prime_factor(
-            num=600851475143
+            num=int(num)
         )
         return problem_3_soln
 
-    def problem_4(self):
+    def problem_4(self, lower=100, upper=999):
         # Instrution: Largest Palindromic Number as product of two 3-digit numbers
         problem_4_soln = CustomEulerFunction().get_largest_palindromic_number_as_product(
-            lower=100,
-            upper=999
+            lower=int(lower),
+            upper=int(upper)
         )
         return problem_4_soln
 
@@ -52,40 +52,24 @@ class Euler:
 
     def problem_6(self, lower: int, upper: int):
         # Instruction: Sum Square Difference: Find the difference between the sum of the squares of the first 100 natural numbers and the square of that sum.
-        sum_of_squares = 0
-        for x in range (lower, upper+1):
-            sum_of_squares+=Math().square(x)
+        problem_6_soln = CustomEulerFunction().get_sum_square_difference(
+            lower=int(lower),
+            upper=int(upper)
+        )
+        return problem_6_soln
 
-        square_of_sums = 0
-        for x in range(lower, upper+1):
-            square_of_sums += x
-        square_of_sums = Math().square(square_of_sums)
-
-        self.logger.info(sum_of_squares)
-        self.logger.info(square_of_sums)
-        return square_of_sums - sum_of_squares
-
-    def problem_7(self, pos: int):
+    def problem_7(self, n=1001):
         # Instruction: 10001st Prime
-        val = 1
-        ctr = 0
-        while ctr < pos+1:
-            if Math().is_prime(val):
-                ctr+=1
-            if ctr == pos:
-                return val
-            val+=1
+        problem_7_soln = CustomEulerFunction().get_nth_prime(
+            n = int(n)
+        )
+        return problem_7_soln
 
-    def problem_8(self, adj_digits: int):
-        big_number = 7316717653133062491922511967442657474235534919493496983520312774506326239578318016984801869478851843858615607891129494954595017379583319528532088055111254069874715852386305071569329096329522744304355766896648950445244523161731856403098711121722383113622298934233803081353362766142828064444866452387493035890729629049156044077239071381051585930796086670172427121883998797908792274921901699720888093776657273330010533678812202354218097512545405947522435258490771167055601360483958644670632441572215539753697817977846174064955149290862569321978468622482839722413756570560574902614079729686524145351004748216637048440319989000889524345065854122758866688116427171479924442928230863465674813919123162824586178664583591245665294765456828489128831426076900422421902267105562632111110937054421750694165896040807198403850962455444362981230987879927244284909188845801561660979191338754992005240636899125607176060588611646710940507754100225698315520005593572972571636269561882670428252483600823257530420752963450
-        list_big_number = list(str(big_number))
-        list_products = []
-        for x in range(0, len(list_big_number)-adj_digits):
-            tmp_prod = 1
-            for y in range(1, adj_digits+1):
-                tmp_prod *= int(list_big_number[int(x)+int(y)])
-            list_products.append(tmp_prod)
-        return max(list_products)
+    def problem_8(self, num_adjacent_digits=4):
+        problem_8_soln = CustomEulerFunction().get_largest_prod_in_series(
+            num_adjacent_digits=int(num_adjacent_digits)
+        )
+        return problem_8_soln
 
     def problem_9(self, triplet_sum: int):
         for x in range(1, triplet_sum-3):
--- src/support/custom_euler_functions.py
+++ src/support/custom_euler_functions.py
@@ -29,19 +29,6 @@ class CustomEulerFunction:
         prime_factors = Math().get_prime_factors(num)
         return max(list(prime_factors))
 
-    """def get_largest_palindromic_number_as_product(self, lower: int, upper: int):
-        # Function for Euler Problem 4
-        palindromic_prods = []
-        for x in range(upper-1, lower, -1):
-            for y in range(upper-1, lower, -1):
-                product = x*y
-                if Math().check_palindrome(product):
-                    palindromic_prods.append(product)
-        if palindromic_prods:
-            return max(palindromic_prods)
-        else:
-            return None"""
-
     def get_largest_palindromic_number_as_product(self, lower: int, upper: int):
         # Function for Euler Problem 4
         set_a = list(range(lower, upper+1))
@@ -49,16 +36,6 @@ class CustomEulerFunction:
         cross_prod = [i*j for i in set_a for j in set_b]
         palindromes = filter(lambda i: Math().check_palindrome(i), cross_prod)
         return max(palindromes)
-
-
-    """def get_smallest_multiple_of_range(self, lower: int, upper: int):
-        # Function for Euler Problem 5
-        max_value = 1
-        for x in range(lower, upper+1):
-            max_value *= x
-        for x in range(lower, max_value):
-            if Math().check_divisors(lower, upper, x):
-                return x"""
 
     def get_smallest_multiple_of_range(self, lower: int, upper: int):
         multi_range = list(range(lower, upper+1))
@@ -69,16 +46,38 @@ class CustomEulerFunction:
                 range_factor_dict[x] = list(Math().get_prime_factors(x))
         self.logger.info(range_factor_dict)
 
+    def get_sum_square_difference(self, lower: int, upper:int):
+        # Function for Euler Problem 6
+        sum_of_squares = 0
+        for x in range (lower, upper+1):
+            sum_of_squares+=Math().square(x)
 
-        """prime_multi_range = list(filter(lambda x: Math().is_prime(x), multi_range))
-        self.logger.info(f"Primes: {prime_multi_range}")
+        square_of_sums = 0
+        for x in range(lower, upper+1):
+            square_of_sums += x
+        square_of_sums = Math().square(square_of_sums)
 
-        i = 1
-        while i > 0:
-            # Perform same mod operation to entire list of primes
-            result = list(map(lambda x : i % x, mul_range))
-            # Check if set is exactly 0
-            list_result = list(set(result))
-            if len(list_result) == 1 and list_result[0] == 0:
-                return i
-            i += 1"""
+        self.logger.info(sum_of_squares)
+        self.logger.info(square_of_sums)
+        return square_of_sums - sum_of_squares
+
+    def get_nth_prime(self, n: int):
+        val = 1
+        ctr = 0
+        while ctr < n+1:
+            if Math().is_prime(val):
+                ctr+=1
+            if ctr == n:
+                return val
+            val+=1
+
+    def get_largest_prod_in_series(self, num_adjacent_digits: int):
+        big_number = 7316717653133062491922511967442657474235534919493496983520312774506326239578318016984801869478851843858615607891129494954595017379583319528532088055111254069874715852386305071569329096329522744304355766896648950445244523161731856403098711121722383113622298934233803081353362766142828064444866452387493035890729629049156044077239071381051585930796086670172427121883998797908792274921901699720888093776657273330010533678812202354218097512545405947522435258490771167055601360483958644670632441572215539753697817977846174064955149290862569321978468622482839722413756570560574902614079729686524145351004748216637048440319989000889524345065854122758866688116427171479924442928230863465674813919123162824586178664583591245665294765456828489128831426076900422421902267105562632111110937054421750694165896040807198403850962455444362981230987879927244284909188845801561660979191338754992005240636899125607176060588611646710940507754100225698315520005593572972571636269561882670428252483600823257530420752963450
+        list_big_number = list(str(big_number))
+        list_products = []
+        for x in range(0, len(list_big_number)-num_adjacent_digits):
+            tmp_prod = 1
+            for y in range(1, num_adjacent_digits+1):
+                tmp_prod *= int(list_big_number[int(x)+int(y)])
+            list_products.append(tmp_prod)
+        return max(list_products)
